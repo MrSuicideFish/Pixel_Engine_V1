cbc800930cc18f83bef909dc1e13b3710e1cef05
Malcolm Neal <Malcolm259@Gmail.com>
2014-10-25 16:04:28 -0400

    Added dependencies

--- PixelEngineProj/PixelEngineEditor/Form1.cs
+++ PixelEngineProj/PixelEngineEditor/Form1.cs
@@ -7,11 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SFML.Window;
+using SFML.Graphics;
+using SFML.Audio;
 
 namespace PixelEngineEditor {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            //create a drawing surface
+
+            RenderWindow renderWindow = new RenderWindow(VideoMode.DesktopMode, "Sometihng");
+            renderWindow.Display();
+
+            //EditorViewportContainer.Controls.Add();
         }
     }
 }
--- PixelEngineProj/PixelEngineEditor/Program.cs
+++ PixelEngineProj/PixelEngineEditor/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SFML.Window;
+using SFML.Audio;
+using SFML.Graphics;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace PixelEngineEditor {
     static class Program {
